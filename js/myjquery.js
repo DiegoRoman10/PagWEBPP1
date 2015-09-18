ce13af265dd25655677c4cc8334bfe610bf393e8
diegorg95 <roman.diegocw@gmail.com>
2015-09-18 18:30:57 -0500

    comentario

---
+++ js/myjquery.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+// Closes the sidebar menu
+    $("#menu-close").click(function(e) {
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
+    });
+	    // Opens the sidebar menu
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
+    });
+	    // Scrolls to the selected menu item on the page
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+	                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+     $('img.iconef').on('mouseover', function(){
+                $(this).toggleClass('animated pulse');
+            });
+    $('.car').slick({
+        setting-name: setting-value
+     });
+    $('.fade').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+    /*
+	});*/
+});
